b9784be987803361d7979019bc36bf8784a77223
preethi412 <preethi4612@gmail.com>
2026-08-10 03:51:34 +0530

    Day 2 assignment

---
+++ Day 2/DataTypes.js
@@ -0,0 +1,19 @@
+var firstName = 'Preethi'
+console.log('The fisrtname is ', firstName);
+console.log(typeof firstName);
+
+var companyName = 'CG'
+console.log('The company name is ', companyName);
+console.log(typeof companyName);
+
+var phoneNo = 8756549998
+console.log(phoneNo);
+console.log(typeof phoneNo);
+
+var isAutomation = true;
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
